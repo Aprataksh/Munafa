41e0f7402d7a6bee9710807d1a5d140b56de00e4
Rohit <rohit.verma.email@gmail.com>
2018-05-23 22:09:23 +0530

    add the ability to append newly downloaded data to historical data; more documentation
    add the ability to append a newly downloaded data to the historical data.
    
    One problem is that the newly downloaded data has a header which also gets appended to the historical data. Possible ways to resolve this included reading the files ussing a pandas data. frame or using a CSV reader and writer. in the former case, the code ends up depending the row number to every row. In the latter case, the writer ends up adding a blank row.
    
    So, for now, we will live with the multiple headers being a part of historical data.
    
    Also added comments in the global configuration settings.
    
    Unit test: checked and validated that the data was being appended to a small subset of NSE 500 stock data..
    
    Also downloaded fresh new data for different intervals..

--- stock data download/stock_tocsv.py
+++ stock data download/stock_tocsv.py
@@ -1,3 +1,16 @@
+'''
+this code is based on the following post
+http://dacatay.com/data-science/download-free-intraday-stock-data-from-google-finance-with-python/
+
+the main differences are that we take care of any company  which has a & in its name and
+the append the newly downloaded data to historical data give the appropriate flag has been
+added in the source code. note that while appending, the headers of datetime, close high etc..
+are also a appended.different approaches including reading the file into a pandas data frame or
+using a csv reader and writer work tried to work around this. In the former case,  the pandas data frame
+ends up prepending the row numbertto every row.  In the latter, the writer ends up adding a blank row.
+So for now, we will live with multiple headers being a part of historical data
+'''
+
 import csv
 import datetime
 import re
@@ -47,14 +60,24 @@ def get_ticker_list():
         print(ticker_list)
 
 adj_list = []
-
+'''
+global configuration settings
+'''
+# interval in seconds  - for 5 minutes, use 60 seconds, for 15-900, for 30-1800, 60-3600
 period = 300
-days = 2
+# number of days in the past for which to download data. Maximum seems to be 50
+days = 1
+# name of the exchange. For India, use NSE
 exchange = 'NSE'
+# should be appended the newly downloaded data in CSV files to historical data? Enter 1 for yes
 append_to_historical_data = 1
+
+# modify the directory path below according to your requirements
+# Note: these directories should already be existing in your file system
 path_to_historical_data = "C:/Users/Rohit/Python_source_code/test_nse 500 historical data/"
-path_to_cur_stock_data_directory = "C:/Users/Rohit/Python_source_code/current_stock_data/"
-path_to_stock_master_list = "C:/Users/Rohit/Python_source_code/nse 500 stock data/test_nifty500_list.csv"
+path_to_cur_stock_data_directory = "C:/Users/Rohit/Python_source_code/test_current_stock_data/"
+# the list that contains the symbols for all the stocks that need to be downloaded
+path_to_stock_master_list = "C:/Users/Rohit/Python_source_code/list of stocks/test_nifty500_list.csv"
 
 get_ticker_list()
 for ticker in ticker_list:
@@ -63,19 +86,28 @@ for ticker in ticker_list:
     print(ticker)
     df = get_google_finance_intraday(ticker, exchange, period=period, days=days)
     df.to_csv(path_to_cur_stock_data_directory + ticker + ".csv")
+    '''
+    when we read the data in the data frame, it also prepends every row with the row number
+    '''
     if (append_to_historical_data == 1):
-        historical_df = pd.read_csv(path_to_historical_data+ ticker + ".csv")
-        historical_df.append(df)
-        historical_df.to_csv(path_to_historical_data+ ticker + ".csv")
-        '''
-        with open(path_to_historical_data+ ticker + ".csv", 'a') as f:
-            with open(path_to_cur_stock_data_directory + ticker + ".csv", 'r') as tempfile:
-                row_file_writer = csv.writer(f)
+        historical_df = pd.read_csv(path_to_historical_data + ticker + ".csv")
+        appended_data = historical_df.append(df)
+        appended_data.to_csv(path_to_historical_data + ticker + " backup2"+".csv")
+    '''
+
+    with open(path_to_historical_data+ ticker + ".csv", 'a') as f:
+        with open(path_to_cur_stock_data_directory + ticker + ".csv", 'r') as tempfile:
+            '''
+            Using csv.reader and a writer along with the writerow does not work because
+            while writing, and extra blank row is being added
+            
+            row_file_writer = csv.writer(f)
                 lines = csv.reader(tempfile)
                 for line in lines:
                     if "Close" not in line:
                         row_file_writer.writerow(line)
-                #f.write(tempfile.read())
-                '''
+             '''
+                f.write(tempfile.read())
+
 
 
