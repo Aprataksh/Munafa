4230d27670844076c3d549f39de96d2dbaa604b7
Rohit <rohit.verma.email@gmail.com>
2018-05-24 13:20:28 +0530

    alert the user to forthcoming dates of weekly and monthly results
    alert the user to forthcoming dates of weekly and monthly results. If the user provides a list of symbols, it will also tell if any of the forthcoming results pertain to the companies whose symbols have been included by the user in his list.
    
    The data is downloaded from the NSE website.
    
    Co-Authored-By: Aayushmaan18 <aayushmaan18@users.noreply.github.com>

--- results data alert/results date alert.py
+++ results data alert/results date alert.py
@@ -1,50 +1,61 @@
 import csv
 import datetime
+import requests
+import codecs
 
-ticker_list_in_week = []
 weekday = int(datetime.date.today().strftime("%w"))
 today = datetime.date.today().day
 month = datetime.date.today().month
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 def get_ticker_list_in_week():
-    with open(r"C:\Users\microsoft\Desktop\Stock Market\venv\BM_Latest_Announced.csv",'r') as f:
-        lines = csv.reader(f)
-        for line in lines:
-            if "Symbol" not in line and "Financial Results" in line[3] :
-                str = line[4]
-                intstr = int(str[0] + str[1])
-                m = str[3:6]
-                if m == months[month - 1] and 0 <= intstr - today <= 7 - weekday:
-                    ticker_list_in_week.append(line[0])
-    print("Scheduled in current week\n",ticker_list_in_week,'\n')
+    print('Scheduled in current week\n')
+    page = requests.get(url)
+    lines = csv.reader(codecs.iterdecode(page.content.splitlines(), "utf-8"))
+    for line in lines:
+        if "Symbol" not in line and "Financial Results" in line[3] :
+            str = line[4]
+            intstr = int(str[0] + str[1])
+            m = str[3:6]
+            if m == months[month - 1] and 0 <= intstr - today <= 7 - weekday:
+                print(line[0],'  ',line[1],'\n')
 
-ticker_list_in_month = []
 def get_ticker_list_in_month():
-    with open(r"C:\Users\microsoft\Desktop\Stock Market\venv\BM_Latest_Announced.csv",'r') as f:
-        lines = csv.reader(f)
-        for line in lines:
-            if "Symbol" not in line and "Financial Results" in line[3] :
-                str = line[4]
-                intstr = int(str[0] + str[1])
-                m = str[3:6]
-                if m == months[month - 1]:
-                    ticker_list_in_month.append(line[0])
-    print("Scheduled in current month\n",ticker_list_in_month)
+    print('\nScheduled in current month\n')
+    url = 'https://www.nseindia.com/corporates/datafiles/BM_Latest_Announced.csv'
+    page = requests.get(url)
+    lines = csv.reader(codecs.iterdecode(page.content.splitlines(), "utf-8"))
+    for line in lines:
+        if "Symbol" not in line and "Financial Results" in line[3] :
+            str = line[4]
+            intstr = int(str[0] + str[1])
+            m = str[3:6]
+            if m == months[month - 1]:
+               print(line[0],'  ',line[1],'\n')
 
 def get_same_ticker():
     tucker_list_temp = []
-    print()
-    with open(r"C:\Users\microsoft\Desktop\Stock Market\venv\symbols.csv",'r') as g:
+    print('\nCompanies that are common\n')
+    with open(path_to_stock_master_list,'r') as g:
         lines = csv.reader(g)
         for line in lines:
             tucker_list_temp.append(line[0])
-    with open(r"C:\Users\microsoft\Desktop\Stock Market\venv\BM_Latest_Announced.csv",'r') as f:
-        lines = csv.reader(f)
-        for line in lines:
-            if line[0] in tucker_list_temp :
-                print(line[0]," ",line[4],'\n')
+    page = requests.get(url)
+    lines = csv.reader(codecs.iterdecode(page.content.splitlines(), "utf-8"))
+    for line in lines:
+        if line[0] in tucker_list_temp :
+            print(line[0]," ",line[4],'\n')
+
+'''
+global configuration settings
+'''
+# the URL from which to download that data
+url = 'https://www.nseindia.com/corporates/datafiles/BM_Latest_Announced.csv'
+
+# the list that contains the symbols for all the stocks that need to be downloaded
+path_to_stock_master_list = "C:/Users/Rohit/Python_source_code/list of stocks/test_nifty500_list.csv"
 
 get_ticker_list_in_week()
 get_ticker_list_in_month()
 get_same_ticker()
+
