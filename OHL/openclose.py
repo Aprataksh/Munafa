401596002b5d4e9cf820b5b7986c5817066945d0
Rohit <rohit.verma.email@gmail.com>
2018-05-31 13:02:28 +0530

    fix a bug to calculate the overall cost and overall sell
     fixed that bug to calculate the overall cost and overall sell. It was being calculated by looking adding the running total cost and total sell.
    
    This is incorrect as we are repeatedly adding earlier transactions. Instead, substituted with the total cost of the current transaction and added to the overall cost. Repeat the same procedure for the sell transaction.
    
    Unit test: run the code and ensure that the overall cost is approximately equal to Max capital multiplied by the total number of transactions. For example if the number of buy transactions is 13 and the maximum capitalist 10,000 per transaction, then the overall cost should be equal to the 130,000. Previously it was coming to be around 180,000. This one was made for FMCG stocks from the period of March 13 until May 29 for the five-minute candle.

--- OHL/openclose.py
+++ OHL/openclose.py
@@ -18,6 +18,7 @@ class OHL():
     c_total_sellEOD = 0
     c_total_wins = 0
     c_total_stoploss = 0
+    c_overall_buy_trans = 0
 
     def __init__(self, mpv, mdfo, mvv, mcfsb, tp,sl):
         self.max_price_volatility = mpv
@@ -50,11 +51,13 @@ class OHL():
     def buy_stocks(self,purchase_cost,index_close):
         bought = int(self.max_capital_for_single_buy / purchase_cost)
         if bought != 0:
-            self.total_cost = self.total_cost + bought * purchase_cost
+            self.total_cost = self.total_cost + (bought * purchase_cost)
             self.total_purchased+=bought
-            self.overall_cost+=self.total_cost
+            self.overall_cost = self.overall_cost + (bought * purchase_cost)
+            self.c_overall_buy_trans = self.c_overall_buy_trans + 1
             print("Stock " + self.line[2] + " bought " + str(bought) + " shares at " + str(purchase_cost) + " price at date " + str(self.rowslist[index_close])[2:12] + " at time " + str(self.rowslist[index_close])[12:18] + "\n")
         else:
+            # this may happen if the price of one stock is more than the  maximum capital
             print("Could not buy stock " + self.line[2] + " on date " + str(self.rowslist[index_close])[2:12] + " due to insufficient daily stock fund\n")
         return bought
 
@@ -62,7 +65,7 @@ class OHL():
         per = (float(self.rowslist[index][2]) - close_price) / close_price
         if per >= self.target_price:
             self.total_sell = self.total_sell + bought * float(self.rowslist[index][2])
-            self.overall_sell+=self.total_sell
+            self.overall_sell = self.overall_sell + (bought * close_price)
             self.total_sold+=bought
             print("Stock " + self.line[2] + " sold " + str(bought) + " shares at " + self.rowslist[index][2] + " price at date " + str(self.rowslist[index])[2:12] + " at time " + str(self.rowslist[index])[13:18] + "\n")
             self.overall_c_wins = self.overall_c_wins + 1
@@ -89,13 +92,16 @@ class OHL():
         print("Stock " + self.line[2] + " sold " + str(bought) + " shares at " + self.rowslist[index][1] + " price at date " + str(self.rowslist[index])[2:12] + " at time " + str(self.rowslist[index])[13:18] + "\n")
         self.overall_c_sellEod = self.overall_c_sellEod + 1
         self.c_total_sellEOD = self.c_total_sellEOD + 1
+
     def OHL(self):
         # the list that contains the symbols for all the stocks that need to be downloaded
         path_to_stock_master_list = "C:/Users/Rohit/Python_source_code/list of stocks/modified_ind_nifty50list.csv"
+        #path_to_stock_master_list = "C:/Users/Rohit/Python_source_code/list of stocks/ind_niftyfmcglist.csv"
 
         path_to_historical_data = "C:/Users/Rohit/Python_source_code/historical_stock_5_min_data/"
 
         path_to_index_file = "C:/Users/Rohit/Python_source_code/historical_indices_5_min_data/NIFTY.csv"
+        #path_to_index_file = "C:/Users/Rohit/Python_source_code/historical_indices_5_min_data/CNXFMCG.csv"
 
         with open(path_to_stock_master_list, 'r') as f:
             self.lines = csv.reader(f)
@@ -118,23 +124,31 @@ class OHL():
                             date = '0'
                             while index < len(self.rowslist):
                                 row = self.rowslist[index]
+                                # do not read data which contains  headers such as "Close"
                                 if 'Close' in row:
                                     index+=1
                                     continue
                                 if row[0][8:10] != date[6:8]:
+                                    # encountered a new date; initiate algorithm for checking the day open price
                                     bought = 0
                                     c = 0
                                     date = str(row[0])[2:10]
                                     index_open = index
+                                    # record the day open price
                                     open_price = float(row[4])
+                                # check for the price at 10 AM and ...
                                 if date + " 10:00:00" in row[0]:
+                                    # record the close price of the 10 AM candle
                                     close_price = float(row[1])
                                     index_close = index
                                     c = 1
+                                # check if we have not seen any price  between 10-11 AM
+                                # this may happen if there has been no trading
                                 if date + " 11:" in row[0] and c == 0:
                                     print("No data for stock " + self.line[2] + " on date " + str(row)[2:12] + "\n")
                                     c = 2
                                 if c == 1:
+                                    # check if the day Open price is almost equal to the close price of the candle at 10 AM
                                     if 0 <= ((close_price - open_price) / open_price) <= self.max_deviation_from_open:
                                         f = 0
                                         if self.check_price_volatility(index_open,index_close): 
@@ -161,14 +175,15 @@ class OHL():
                             print("Total wins = " + str(self.c_total_wins) + " Total sell EOD = " + str(self.c_total_sellEOD) + " Stop Loss = " + str(self.c_total_stoploss) )
         print("Total purchases = "+str(self.total_purchased)+" total sold = "+str(self.total_sold)+"\n")
         print("Overall cost = "+str(self.overall_cost)+" Overall sell = "+str(self.overall_sell)+"\n")
-        print("Overall wins = " + str(self.overall_c_wins) + " Total sell EOD = " + str(self.overall_c_sellEod) + " Overall stoploss = " + str(self.overall_c_stoploss))
+        print("Overall buys = " + str(self.c_overall_buy_trans) + " Overall wins = " + str(self.overall_c_wins) + " Total sell EOD = " + str(self.overall_c_sellEod) + " Overall stoploss = " + str(self.overall_c_stoploss))
 def main():
     max_price_volatility = 0.02
     max_deviation_from_open= 0.005
     max_volume_volatility = 2
     max_capital_for_single_buy=10000
     target_price=0.005
-    stop_loss=0.01
+    #the stoploss needs to be negative.
+    stop_loss=-0.01
     obj=OHL(max_price_volatility,max_deviation_from_open,max_volume_volatility,max_capital_for_single_buy,target_price,stop_loss)
     obj.OHL()
 
