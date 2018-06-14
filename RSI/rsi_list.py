9010298eb009d6260a2584a8376648db21965587
Rohit <rohit.verma.email@gmail.com>
2018-06-14 09:57:20 +0530

    verify the RSI calculation
    verify the RSI calculation by comparing them with the calculations done in Excel  at http://investexcel.net/relative-strength-index-spreadsheet/.
    
    The data that has been used is also committed. The Excel macro file is the one that has been downloaded from the website mentioned above. The data files from Bajaj financial services were modified to include the prices from the Excel file and the calculations as a result of running the Python script were compared with that in the Excel file.
    
    As can be inferred from the files, with RSI you 14, the initial values differ but the difference becomes progressively less and eventually becomes around 0.02 as we approach the end of the file which is about 103 rows long.
    
    this code has been authored by Aayushmaan Rajora

---
+++ RSI/rsi_list.py
@@ -0,0 +1,47 @@
+import csv
+import sys
+import pandas as pd
+from stockstats import StockDataFrame as Sdf
+import matplotlib.pyplot as plt
+sys.path.insert(0, r"..\utilities")
+import config
+import get_ticker_list
+
+class RSI:
+    obj = config.config(r"../config.txt")
+
+    def __init__(self,p,ticker,path):
+        self.period=p
+        self.ticker=ticker
+        self.path_to_ticker_data=path
+
+    def calculate_rsi(self):
+        rows = []
+        times = []
+        columns = ['Close', 'High', 'Low', 'Open', 'Volume']
+        with open(self.path_to_ticker_data,'r') as f:
+            lines = csv.reader(f)
+            for line in lines:
+                if 'Close' not in line:
+                    times.append(line[0])
+                    rows.append(map(float,line[1:]))
+            df = pd.DataFrame(rows, columns=columns)
+            stock_df = Sdf.retype(df)
+            rsi_text="rsi_"+str(self.period)
+            df['rsi'] = stock_df[rsi_text]
+            rsi=list(df['rsi'])
+            rsi_df=pd.DataFrame(rsi,index=pd.DatetimeIndex(times, name='Date Time'),columns=['RSI'])
+            #print(rsi_df)
+            rsi_df.to_csv(self.obj.path_to_output_dir() + self.ticker + "_rsi.csv")
+            #plt.plot(rsi_df['RSI'],color='red')
+            #plt.show()
+
+def main():
+    period=14
+    obj = config.config(r"../config.txt")
+    ticker_list=get_ticker_list.get_ticker_list(obj.path_to_master_list())
+    for ticker in ticker_list:
+        print(ticker)
+        rsi_obj=RSI(period,ticker,obj.path_to_historical_5_min_dir()+ticker+".csv")
+        rsi_obj.calculate_rsi()
+main()
