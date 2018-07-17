48d254867e2117be4c5aca585868c60979889b16
Rohit <rohit.verma.email@gmail.com>
2018-07-17 14:00:12 +0530

    by the data reader in order to implement the opening range breakout
    in order to implement the opening range breakout, we need tomodify the code for trade entry index to make it for generic. Similarly the for trade exit index has also been modified
    
    Unit test: run this code along with the opening range breakout code and validate the trades visually
    
    This file should have been committed along with the code for opening range breakdout
    
    This code has been committed by Aayushmaan Rajora

--- Utilities/data_reader.py
+++ Utilities/data_reader.py
@@ -14,49 +14,59 @@ class data_reader():
         if self.trade_entry_index < len(self.trade_entry_rowslist):
             row = self.trade_entry_rowslist[self.trade_entry_index]
         return row
+
     def get_prev_row_for_trade_entry(self):
         row = []
         if self.trade_entry_index < len(self.trade_entry_rowslist):
-            row = self.trade_entry_rowslist[self.trade_entry_index-1]
+            row = self.trade_entry_rowslist[self.trade_entry_index - 1]
         return row
+
     def get_row_for_trade_exit(self):
         row = []
         if self.trade_exit_index < len(self.trade_exit_rowslist):
             row = self.trade_exit_rowslist[self.trade_exit_index]
         return row
+
     def get_prev_row_for_trade_exit(self):
         row = []
         if self.trade_exit_index < len(self.trade_exit_rowslist):
-            row = self.trade_exit_rowslist[self.trade_exit_index-1]
+            row = self.trade_exit_rowslist[self.trade_exit_index - 1]
         return row
+
     def move_to_next_row(self):
         if self.entry_or_exit == False:
             self.trade_entry_index = self.trade_entry_index + 1
         else:
             self.trade_exit_index = self.trade_exit_index + 1
+
+    def find_date_in_date_list(self,date_to_find,search_start_index,date_list):
+        date_to_find = datetime.strptime(date_to_find[2:], "%y-%m-%d %H:%M:%S")
+        for i in range(search_start_index, len(date_list)):
+            date = datetime.strptime(date_list[i][self.datetime_column][2:], "%y-%m-%d %H:%M:%S")
+            if date_to_find <= date:
+                return i
+        return -1
+
     def init_trade_exit_index(self,date):
-        trade_entry_time = datetime.strptime(date[2:], "%y-%m-%d %H:%M:%S")
-        for i in range(1,len(self.trade_exit_rowslist)):
-            trade_exit_time = datetime.strptime(self.trade_exit_rowslist[i][self.datetime_column][2:], "%y-%m-%d %H:%M:%S")
-            if trade_entry_time <= trade_exit_time:
-                self.trade_exit_index = i
-                break
+        self.trade_exit_index = self.find_date_in_date_list(date,1,self.trade_exit_rowslist)
+        if self.trade_exit_index == -1:
+            self.trade_exit_index = len(self.trade_exit_rowslist) - 1
+
     def init_trade_entry_index(self):
-        trade_entry_time = datetime.strptime(self.trade_entry_rowslist[self.trade_entry_index][self.datetime_column][2:], "%y-%m-%d %H:%M:%S")
-        trade_exit_time = datetime.strptime(self.trade_exit_rowslist[self.trade_exit_index][self.datetime_column][2:], "%y-%m-%d %H:%M:%S")
-        while trade_entry_time < trade_exit_time:
-            self.trade_entry_index = self.trade_entry_index + 1
-            if self.trade_entry_index >= len(self.trade_entry_rowslist):
-                break
-            trade_entry_time = datetime.strptime(self.trade_entry_rowslist[self.trade_entry_index][self.datetime_column][2:], "%y-%m-%d %H:%M:%S")
+        self.trade_entry_index = self.find_date_in_date_list(self.trade_exit_rowslist[self.trade_exit_index][0],self.trade_entry_index,self.trade_entry_rowslist)
+        if self.trade_entry_index == -1:
+            self.trade_entry_index = len(self.trade_entry_rowslist) - 1
+
     def check_trade_exit_eof(self):
         if self.trade_exit_index == len(self.trade_exit_rowslist) - 1:
             return True
         else:
             return False
+
     def read_trade_entry_data(self):
         with open(self.obj.path_to_trade_entry_dir() + self.line[self.column_no_of_ticker] + ".csv", 'r') as g:
             self.trade_entry_rowslist = list(csv.reader(g))
+
     def read_trade_exit_data(self):
         with open(self.obj.path_to_trade_exit_dir() + self.line[self.column_no_of_ticker] + ".csv", 'r') as g:
             self.trade_exit_rowslist = list(csv.reader(g))
